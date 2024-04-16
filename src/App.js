2f61ab3a10227554b829158ea68b4349a52e107a
Gifty <giftyarthur133@gmail.com>
2024-04-16 14:01:56 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components";
+import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar';
+
+export default function App() {
+  return (
+    <div>
+      <Sidebar/>
+      <Dashboard/>
+    </div>
+  )
+}
+
+const Div =  styled.div ``;
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from './Navbar';
+import Analytics from './Analytics';
+import FAQ from "./FAQ";
+import Earnings from './Earnings';
+import Transfers from './Transfers';
+import Profile from './Profile';
+
+export default function Dashboard() {
+  return (
+    <Section>
+      <Navbar/>
+      <div className="grid">
+        <div className="row_one">
+          <Analytics/>
+          <FAQ />
+        </div>
+        <div className="row_two">
+          <Earnings/>
+          <Transfers/>
+          <Profile/>
+       
+        </div>
+      </div>
+    </Section>
+  )
+}
+
+const Section = styled.section``;
